64e0c2d78344942d17681eec9ffd1429ad3ed0b8
Will <will@fingersonezero.com>
2022-04-28 13:18:34 +0800

    OMS Authenticate

--- util/auth/auth.go
+++ util/auth/auth.go
@@ -33,3 +33,21 @@ func Authenticate(token string) (id int32, err error) {
 
 	return clm.ID, errors.Wrap(err, "")
 }
+
+
+// OMSAuthenticate 鉴权 JWT token.
+func OMSAuthenticate(token string) (id int64, tenantID int64, err error) {
+  // JWT Secret 存储在环境变量里面的密钥.
+  jwtSecret := env.JwtSecret
+
+  if jwtSecret == "" {
+    jwtSecret = "wms"
+  }
+
+  clm, err := ParseOMSToken(token, jwtSecret)
+  if err != nil || clm == nil {
+    return 0, 0, internal.NewError("failed to parse token", http.StatusUnauthorized)
+  }
+
+  return clm.ID, clm.TenantID, errors.Wrap(err, "")
+}
