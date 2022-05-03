d171181f9f2ca5a5eb0852ca2f287a3efe85f1dd
Will <will@fingersonezero.com>
2022-05-03 18:40:28 +0800

    Add helpers

---
+++ util/const.go
@@ -0,0 +1,22 @@
+package util
+
+// Sentry DSN.
+
+const (
+  SentrySyncTimeout = 3
+)
+
+// HTTP.
+
+const (
+  CorsMaxAge      = 300
+  EntryTimeLength = 100
+)
+
+const (
+  OMSUserType = "customer"
+)
+
+const (
+  SystemTenantID = int64(1)
+)
